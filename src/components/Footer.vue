e13cf709f4b3083a69629bbd4510b876a9c16e06
Jonatas Luis Prates <CIANDT\jonatas.prates@lnb035629cps.ciandt.global>
2022-07-27 18:31:12 -0300

    fazendo requisição axios get para trazer dados do footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,35 @@
 <template>
     <footer>
-        asd
+        <a class="socialLink" v-for="(socialLink,index) in socialLinks" :key="index"
+        :href="socialLink.link">
+           <img :src="socialLink.icon" :alt="socialLink.label" :title="socialLink.label"> 
+        </a>
     </footer>
 </template>
 <script>
+import api from '@/services/api.js';
+
 export default {
     // eslint-disable-next-line vue/multi-word-component-names
-    name: 'Footer'
+    name: 'Footer',
+    
+    //criação de variaveis
+    data() {
+        return {
+            socialLinks: []
+        }
+    },
+
+    //evento criado quando abre a página
+    mounted() {
+        api.get('/social-links.json').then(response => {
+            this.socialLinks = response.data;
+        })
+        .catch(error => {
+            console.log(error.response)
+        });
+    }
+
 }
 </script>
 
@@ -19,6 +42,21 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
+    }
+
+    .socialLink {
+        border: 2px solid var(--color-text-light);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        margin: 0 10px;
+    }
+
+    .socialLink img {
+        width: 20px;
     }
 
 </style>
